f271bdcaf17985e1f414ca6c1742457ab695a48f
Lousy-mage <nihalnavas123@gmail.com>
2024-11-29 11:12:34 +0530

    upto basic interface

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ class Wallet{
     void setBalance(int amount){
         Balance=amount;
     }
-}userBalance;
+};
 
 class Transaction: public Wallet{
     int income;
@@ -22,8 +22,28 @@ class Transaction: public Wallet{
     void addExpense(int amount){
         expense=amount;
     }
-}
+}transactions;
+
 
 int main(){
+
+    int opt;
+    do{
+        cout<<"Welcome to Ledgerous!"<<endl;
+        cout<<"1. View Balance"<<endl;
+        cout<<"2. View Transaction Hitory"<<endl;
+        cout<<"3. Add Transaction"<<endl;
+        cout<<"Choose an Option to Continue: "<<endl;
+        cin>>opt;
+
+        switch(opt){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }while(opt);
     return 0;
 }
